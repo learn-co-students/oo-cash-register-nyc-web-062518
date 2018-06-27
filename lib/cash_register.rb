bbfa7a47e81b60ba554f0ce06b36118fefff3db9
grandballoon <luke.sovern.conley@gmail.com>
2018-06-27 16:45:06 -0400

    cash register lab

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
+require "pry"
+class CashRegister
 
+attr_accessor :discount
+attr_accessor :contents
+attr_accessor :total
+attr_accessor :last_price
+
+def initialize(discount=0)
+  @discount = discount
+  @contents = []
+  @total = 0
+  @last_price = 0
+end
+
+def total
+  @total
+end
+
+def apply_discount
+  if @discount == 0
+    "There is no discount to apply."
+  else
+    @total = @total - @total*@discount/100
+    "After the discount, the total comes to $#{@total}."
+  end
+end
+
+def add_item(name, price, quantity=1)
+  quantity.times do
+    @contents << name
+    self.total += price
+  end
+  @last_price = price
+end
+
+def items
+  @contents
+end
+
+def void_last_transaction
+  @total -= @last_price
+end
+
+end

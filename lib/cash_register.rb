9506d0022bd65e79ffb047893572c24b5c7b60f7
yunglleung1 <yungl.leung1@gmail.com>
2018-06-27 18:10:49 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+# Add items of varying quantities and prices
+# Calculate discounts
+# Keep track of what's been added to it
+# Void the last transaction
+
+class CashRegister
+	
+	def initialize(discount=0)
+		@total = 0
+		@discount = discount
+		@items = []
+		@prices = []
+    end
+
+    attr_reader :total, :discount, :itemName
+    attr_writer :total, :itemName
+  	#attr_accessor :bank_account, :hygiene
+
+  	def add_item(itemName, price, quantity=1)
+  		@total += quantity * price
+  		quantity.times do
+  			@items.push(itemName)
+  			@prices.push(price)
+  		end
+  	end
+
+  	def apply_discount
+  		@total = @total - ((discount.to_f / 100) * @total)
+  		if self.discount != 0 
+  			"After the discount, the total comes to $#{@total.to_i}."
+  		else
+  			"There is no discount to apply."
+  		end
+  	end
+
+  	def items
+  		@items
+  	end
+
+  	def void_last_transaction
+  		@total -= @prices.last
+  	end
+end
+
+
+
+
 
